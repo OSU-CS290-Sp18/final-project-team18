25e2ae3b251bc6bfad6e8b788307c36de082c670
JaredBeale <bealej@oregonstate.edu>
2018-06-09 14:16:03 -0700

    Jared made content delivery system work with json, will update for mongo later

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
+/*var MongoClient = require('mongodb').MongoClient;
 
 var mongoHost = process.env.MONGO_HOST;
 var mongoPort = process.env.MONGO_PORT || 27017;
@@ -12,7 +12,7 @@ var mongoDBName = process.env.MONGO_DB_NAME;
 
 var mongoUrl = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDBName;
 
-var mongoDB = null;
+var mongoDB = null;*/
 
 var app = express();
 var port = process.env.PORT || 3008;
@@ -20,9 +20,15 @@ var port = process.env.PORT || 3008;
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+var subs = require('./subData');
+
 app.use(bodyParser.json());
 
-app.get('/subs/:sub', function (req, res, next) {
+app.get('/', function (req, res, next) {
+  res.status(200).render('homePage', {subList: subs});
+});
+
+/*app.get('/subs/:sub', function (req, res, next) {
   var sub = req.params.sub.toLowerCase();
   var subCollection = mongoDB.collection('subs');
   subCollection.find({subName: sub}).toArray(function (err, people) {
@@ -30,9 +36,15 @@ app.get('/subs/:sub', function (req, res, next) {
     else if (people.length > 0) res.status(200).render('sub', people[0]);
     else next();
   });
+});*/
+
+app.get('/subs/:sub', function (req, res, next) {
+  var sub = req.params.sub.toLowerCase();
+  if (subs[sub]) res.status(200).render('sub', subs[sub]);
+  else next();
 });
 
-app.post('/subs/:sub/addPic', function (req, ers, next) {
+/*app.post('/subs/:sub/addPic', function (req, ers, next) {
   var person = req.params.person.toLowerCase();
   if (req.body && req.body.link && req.body.title) {
     var pic = {
@@ -54,6 +66,23 @@ app.post('/subs/:sub/addPic', function (req, ers, next) {
       });
   }
   else res.status(400).send("Request needs JSON body with link and title");
+});*/
+
+app.post('/subs/:sub/addPic', function (req, res, next) {
+  var sub = req.params.sub.toLowerCase();
+  if (subs[sub]) {
+    if (req.body && req.body.link && req.body.title) {
+      var pic = {
+        link: req.body.link,
+        title: req.body.title,
+        tags: []
+      };
+      subs[sub].pics.push(pic);
+      res.status(200).end()
+    }
+    else res.status(400).send("Request needs a JSON body with link and title");
+  }
+  else next();
 });
 
 app.use(express.static('public'));
@@ -62,11 +91,14 @@ app.get('*', function(req, res) {
   res.status(404).render('404');
 });
 
-MongoClient.connect(mongoUrl, function (err, client) {
+/*MongoClient.connect(mongoUrl, function (err, client) {
   if (err) throw err;
   mongoDB = client.db(mongoDBName);
   app.listen(port, function() {
     console.log("== Server is listening on port ", port);
   });
-});
+});*/
 
+app.listen(port, function() {
+  console.log("== Server is listening on port ", port);
+});
